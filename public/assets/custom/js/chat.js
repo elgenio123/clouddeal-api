b307a36e592c46977cbfe0304de04e3e3b04f7e0
sagesse123 <sagesikoun1@gmail.com>
2023-07-03 18:10:36 +0200

    UPDATE

--- public/assets/custom/js/chat.js
+++ public/assets/custom/js/chat.js
@@ -1,7 +1,10 @@
 window.addEventListener('alpine:init', () => {
     Alpine.data('chatComponent', () => ({
         discussions: [], // Les discussions récupérées depuis le backend
-        searchQuery: '', // La valeur de recherche de l'utilisateur
+        selectedDiscussion: null ,
+        messages: [],
+        discussionMessage: [],
+        discussionId: 1,
 
         fetchDiscussions() {
             fetch(`/chat/${2}`)
@@ -9,10 +12,38 @@ window.addEventListener('alpine:init', () => {
                 .then(data => {
 
                     this.discussions = data.data;
+                    // if (this.discussions.length =! 0 ) {
+                    //     this.discussionId = this.discussionId + 1;
+
+                    // }
+                    this.messages = this.discussions[this.discussionId].messages;
+                    console.log(this.discussions);
+                    console.log(this.discussionId);
+                    console.log('End of fetch disccusions');
                 })
                 .catch(error => {
                     console.error(error);
                 });
         },
+        // fetchMessages(discussionId) {
+        //     fetch(`/chat/${discussionId}`)
+        //         .then(response => response.json())
+        //         .then(data => {
+
+        //             this.discussionMessage = data.data;
+        //             this.selectedDiscussion= this.discussionMessage.find(discussion => discussion.id === discussionId);
+        //             this.messages = this.selectedDiscussion.messages;
+        //             console.log(this.messages);
+        //             console.log('End of fetch disccusions');
+        //         })
+        //         .catch(error => {
+        //             console.error(error);
+        //         });
+
+
+        // },
+
+
+
     }));
 });
